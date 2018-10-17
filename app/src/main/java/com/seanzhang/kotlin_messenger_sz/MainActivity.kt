c3ade6650ce8e0df6b7ea04158bd1150734351a2
sean <sezhang@aarp.org>
2018-10-16 20:56:56 -0400

    initial commit

---
+++ app/src/main/java/com/seanzhang/kotlin_messenger_sz/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.seanzhang.kotlin_messenger_sz
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
